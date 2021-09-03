ae6cfae883e6c4ccfc8f8921816f10254e17a2d9
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-09-03 11:38:58 +0500

    Installation

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyPipePipe } from './my-pipe.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyPipePipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
