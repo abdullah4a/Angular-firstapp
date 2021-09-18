2c83a68a69d930593f39da3b72a3eb4539db15e3
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-09-18 12:51:10 +0500

    API on working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+class appClass {
+  constructor(
+    public userId: number,
+    public id: number,
+    public title: string,
+    public completed: string
+  ) {}
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'firstapp';
+  app: appClass[] = [];
+  constructor(private httpClient: HttpClient) {}
+  ngOnInit(): void {
+    this.getTodos();
+  }
+  getTodos() {
+    this.httpClient
+      .get<any>('http://jsonplaceholder.typicode.com/todos')
+      .subscribe((responce) => {
+        console.log('Responce ' + responce);
+        this.app = responce;
+        // console.log(this.app);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyPipePipe } from './my-pipe.pipe';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MyPipePipe
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MyPipePipe],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
